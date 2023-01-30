420607e260465728abf300f988630e03b3ee733b
LHexagone <123946288+LHexagone@users.noreply.github.com>
2023-01-30 16:17:58 +0530

    Create Lab_1.java

---
+++ Lab_1.java
@@ -0,0 +1,19 @@
+//Program1 
+
+import java.util.Scanner; 
+
+class Lab_1_X{ 
+    public static void main(){ 
+        Scanner sc = new Scanner(System.in); 
+        int a[] = new int[10];
+			
+        for(int i = 0; i<10; i++){ 
+            System.out.println("Enter a Number"); 
+            a[i] = sc.nextInt(); 
+        } 
+			
+        for(int i = 0; i<10; i++){ 
+            System.out.println(i+”Element is”+ a[i]); 
+        } 
+    } 
+} 
